c7a3308bef5f550030fcb21a9dd31a444dab2b43
fmaraux <60267796+fmaraux@users.noreply.github.com>
2020-04-07 14:47:13 +0200

    debug unexpected argument

--- 2_Select-an-IaC-tool/Terraform/modules/network/network.tf
+++ 2_Select-an-IaC-tool/Terraform/modules/network/network.tf
@@ -128,7 +128,6 @@ resource "azurerm_network_interface" "tf_nic" {
 	name = var.nic_name
 	location = var.location
 	resource_group_name = azurerm_resource_group.tf_rg.name
-	network_security_group_id = azurerm_network_security_group.tf_sg.id
 
 	ip_configuration {
 		name = var.nic_ip_conf_name
