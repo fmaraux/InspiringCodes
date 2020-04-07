e47674bd66fe3024bc28e437c1a720ac0bfe1337
fmaraux <60267796+fmaraux@users.noreply.github.com>
2020-04-07 14:40:06 +0200

    debug argument not expected

--- 2_Select-an-IaC-tool/Terraform/modules/network/network.tf
+++ 2_Select-an-IaC-tool/Terraform/modules/network/network.tf
@@ -52,7 +52,6 @@ resource "azurerm_subnet" "tf_private_subnet" {
 	resource_group_name = azurerm_resource_group.tf_rg.name
 	virtual_network_name = azurerm_virtual_network.tf_vnet.name
 	address_prefix = var.private_sub_address
-	route_table_id = azurerm_route_table.tf_RT.id
 }
 
 #########################
